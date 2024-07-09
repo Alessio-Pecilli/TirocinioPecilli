b6e3d6855d7ac8134da0b8f74e25425439a10081
Alessio Pecilli <57869794+Alessio-Pecilli@users.noreply.github.com>
2024-07-09 21:29:10 +0200

    Update MIX.py

--- MIX.py
+++ MIX.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import readMINST
+import VQSD_QISKIT as vqsd
+import random
+from qutip import Qobj, ket2dm
 from qiskit.quantum_info import Statevector
 import os
 import math
@@ -15,10 +18,25 @@ from qiskit import QuantumCircuit
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
 
+def create_density_matrix(state_vectors):
+    n = len(state_vectors[0].data)  # Dimensione del vettore di stato
+    rho = np.zeros((n, n), dtype=np.complex128)  # Matrice densità inizialmente vuota
+    
+    for statevector in state_vectors:
+        vector = statevector.data  # Ottieni i dati del vettore come array di numpy
+        # Stato puro come matrice densità (prodotto esterno)
+        state = np.outer(vector, np.conjugate(vector))
+        rho += state
+    
+    # Normalizziamo la matrice densità
+    rho /= len(state_vectors)
+    
+    return rho
+
 def FromFileToStateVector(file_path):
     processor = readMINST.MINSTImageProcessor()
-    binary_array = processor.produceArray(file_path, 45)
-    normalizzazione(binary_array)
+    binary_array = processor.produceArray(file_path,random.randint(1, 10000))
+    return normalizzazione(binary_array)
 
 def measure_statevector(quantum_state, num_qubits):
     
@@ -43,9 +61,9 @@ def measure_statevector(quantum_state, num_qubits):
     # Ottieni i risultati
     counts = result.get_counts(transpiled_qc)
 
-    print("Risultati della misura:", counts)
-    plot_histogram(counts)
-    plt.show()
+    #print("Risultati della misura:", counts)
+    #plot_histogram(counts)
+    #plt.show()
 
 def printKet(vectorized_matrix,num_digits):
 
@@ -90,33 +108,35 @@ def normalizzazione(binary_array):
 
     num_qubits = int(np.log2(len(quantum_state)))
     num_digits = num_qubits if num_qubits > 0 else 1
-
-    # Flatten della matrice in un array monodimensionale
-    flattened_matrice = vectorized_matrix.flatten()    
     # Itera attraverso ogni riga della matrice
 
-    # printKet(vectorized_matrix,num_digits)
+    #printKet(vectorized_matrix,num_digits)
 
-    #measure_statevector(quantum_state,num_qubits)
+    measure_statevector(quantum_state,num_qubits)
+
     return quantum_state
 
-def ChooseRandomIMG(rdn):
+def ChooseRandomIMG():
+    rdn = random.randint(1, 2)
     if rdn == 1:
         file_path = ".\\MINST_DATA\\t10k-images.idx3-ubyte"
     elif rdn == 2:
-        file_path = ".\\MINST_DATA\\t10k-labels.idx1-ubyte"
-    elif rdn == 3:
         file_path = ".\\MINST_DATA\\train-images.idx3-ubyte"
-    elif rdn == 4:
-        file_path = ".\\MINST_DATA\\train-labels.idx1-ubyte"
     return file_path
+
+def LoadVector(n):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    risultati = []
+    for _ in range(n):
+        # Scegliere un file casuale
+        file_path = os.path.join(current_dir, ChooseRandomIMG())
+        
+        # Eseguire FromFileToStateVector e salvare il risultato nella lista
+        risultato = FromFileToStateVector(file_path)
+        risultati.append(risultato)
+    return risultati
     
+density_matrix = create_density_matrix(LoadVector(3))
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-f = ChooseRandomIMG(2)
-file_path = os.path.join(current_dir, f)
-FromFileToStateVector(file_path)
-
-
-
-
+#print("Matrice densità quantistica:")
+#print(density_matrix)
